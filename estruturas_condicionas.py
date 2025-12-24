72b9cb3ac6049261f762bc862a4a413d172e1494
luiz <satanaluiz78@gmail.com>
2025-12-24 18:42:19 -0300

    adiciona verificação de idade para acesso

---
+++ estruturas_condicionas.py
@@ -0,0 +1,5 @@
+idade = int(input("Digite sua idade: "))
+if idade >= 18:
+    print("Você é maior de idade.")
+else:
+    print(f" desculpe você não podera acessar pois {idade} nao e permitido, so com 18 anos ou acima .")
