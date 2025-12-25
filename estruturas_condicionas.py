8c125ddb1ca3f869e6f6c3e993ce0418db02940f
luiz <satanaluiz78@gmail.com>
2025-12-24 21:18:35 -0300

    corrige valor do cheque especial de 45o para 450

--- estruturas_condicionas.py
+++ estruturas_condicionas.py
@@ -12,7 +12,7 @@ conta_universitaria = False
 
 saldo = 2000
 saque = 500
-cheque_especial = 45o
+cheque_especial = 450
 if conta_normal:
     if saldo >= saque:
         print('saque realizado com sucesso')
