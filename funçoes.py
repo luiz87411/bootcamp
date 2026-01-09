5da9c3afad63d11dabd9eb085175af257ecaaf60
luiz <satanaluiz78@gmail.com>
2026-01-09 20:42:20 -0300

    funções de saudação com nome fixo e personalizado

---
+++ funçoes.py
@@ -0,0 +1,8 @@
+def saudaçao ():
+    name= "luiz"
+    print(f"Olá! Seja bem-vindo({name})!")
+saudaçao()
+
+def saudaçao_com_nome(luiz):
+    print(f"Olá! Seja bem-vindo({luiz})!")
+saudaçao_com_nome("luiz")
